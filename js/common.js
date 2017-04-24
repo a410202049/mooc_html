deae225b75c275b159e721eb3e0c3f5df65d8785
kerrygao <1509699669@qq.com>
2017-04-24 19:57:06 +0800

    xiugai

--- js/common.js
+++ js/common.js
@@ -9,23 +9,66 @@ $.fn.extend({
     }
 });
 
-$(function(){
 
+//水平垂直居中 支持浏览器窗口大小改变，存在垂直滚动条
+function center(obj) {
+    var screenWidth = $(window).width(), screenHeight = $(window).height();  //当前浏览器窗口的宽高
+    var scrolltop = $(document).scrollTop();//获取当前窗口距离页面顶部高度
+    var objLeft = (screenWidth - obj.width()) / 2;
+    var objTop = (screenHeight - obj.height()) / 2 + scrolltop;
+    obj.css({left: objLeft + 'px', top: objTop + 'px'});
+    //浏览器窗口大小改变时
+    $(window).resize(function () {
+        screenWidth = $(window).width();
+        screenHeight = $(window).height();
+        scrolltop = $(document).scrollTop();
+        objLeft = (screenWidth - obj.width()) / 2;
+        objTop = (screenHeight - obj.height()) / 2 + scrolltop;
+        obj.css({left: objLeft + 'px', top: objTop + 'px'});
+    });
+    //浏览器有滚动条时
+    $(window).scroll(function () {
+        screenWidth = $(window).width();
+        screenHeight = $(window).height();
+        scrolltop = $(document).scrollTop();
+        objLeft = (screenWidth - obj.width()) / 2;
+        objTop = (screenHeight - obj.height()) / 2 + scrolltop;
+        obj.css({left: objLeft + 'px', top: objTop + 'px'});
+    });
+}
+
+$(function(){
 	$('.custom-modal .icon-close').click(function(){
+
 		$(this).parents('.custom-modal').css("display","none");
 	})
 
 	$('#js-signin-btn').click(function(){
-		// console.log(1)
-		// $('#login-modal').css("display","block")
-		
-		// var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-		// $('#login-modal').addClass('animated ' + 'fadeInDown').one(animationEnd, function() {
-
-  //       });
-		// ("#login-modal").animateCss("fadeInDown",function(){
-		// 	console.log(1)
-		// })
-		// $('#login-modal').addClass('fadeInDown animated');
+		$('#login-modal').css("display","block");
+		center($("#login-modal .form-modal"));
+		$("#login-modal .form-modal").animateCss("fadeInDown",function(){})
 	})
+
+	$('#js-register-btn').click(function(){
+		$('#register-modal').css("display","block");
+		center($("#register-modal .form-modal"));
+		$("#register-modal .form-modal").animateCss("fadeInDown",function(){})
+	})
+
+	$('#login_now').click(function(){
+		$('#register-modal').css("display","none");
+		$('#js-signin-btn').click();
+	})
+
+	$('#regsiter_now').click(function(){
+		$('#login-modal').css("display","none");
+		$('#js-register-btn').click();
+	})
+
+
+
 })
+
+
+
+
